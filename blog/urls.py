18bdcef6ed2d26351ab415a4f56f1bce24717485
Erfan Dousti <erfan.flashpoint@gmail.com>
2025-10-27 13:04:59 +0330

    Added models

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("" , views.home, name = "home"),
-    path("posts" , views.posts , name = "posts"),
-    path("posts/<slug:slug>" , views.post , name = "post")
+    path("posts/" , views.posts , name = "posts"),
+    path("posts/<slug:slug>/" , views.post , name = "post")
 ]
